92f16ad90251443d44094f4d15422e2fd6b9e320
m-sef <seth.l.moore0506@gmail.com>
2026-07-28 13:54:52 -0400

    gather_log.sh now globs, changed experiment rates

--- exp/webserver/run_experiment.sh
+++ exp/webserver/run_experiment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for (( i = 10; i < 100; i += 10 )); do
+for (( i = 5; i <= 50; i += 5 )); do
     echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="$i/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/vegeta${i}.log"
 done
 
