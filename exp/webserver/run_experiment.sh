27f4dac6e94fb5015268b4b4b683737eed49165f
m-sef <seth.l.moore0506@gmail.com>
2026-07-28 14:44:07 -0400

    Running new experiment with new RPS range

--- exp/webserver/run_experiment.sh
+++ exp/webserver/run_experiment.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-for (( i = 10; i <= 100; i += 10 )); do
+for (( i = 25; i <= 300; i += 25 )); do
     echo "${i}/s"
     echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/1vegeta${i}.log"
 done
 
-for (( i = 90; i > 0; i -= 10 )); do
+for (( i = 275; i > 0; i -= 25 )); do
     echo "${i}/s"
     echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/2vegeta${i}.log"
 done
