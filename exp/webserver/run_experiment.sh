034de3503dcdba337bbae23237331617c4f5cd95
m-sef <seth.l.moore0506@gmail.com>
2026-07-28 14:54:47 -0400

    Running next experiment; higher peaks

--- exp/webserver/run_experiment.sh
+++ exp/webserver/run_experiment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for (( i = 25; i <= 300; i += 25 )); do
+for (( i = 25; i <= 500; i += 25 )); do
     echo "${i}/s"
     echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/1vegeta${i}.log"
 done
@@ -8,6 +8,16 @@ done
 for (( i = 275; i > 0; i -= 25 )); do
     echo "${i}/s"
     echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/2vegeta${i}.log"
+done
+
+for (( i = 25; i <= 500; i += 25 )); do
+    echo "${i}/s"
+    echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/3vegeta${i}.log"
+done
+
+for (( i = 275; i > 0; i -= 25 )); do
+    echo "${i}/s"
+    echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/4vegeta${i}.log"
 done
 
 exit 0
