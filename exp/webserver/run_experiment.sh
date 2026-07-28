8ca7bc7645adb49331231c4a9de7aafc642c88b9
m-sef <seth.l.moore0506@gmail.com>
2026-07-28 15:37:51 -0400

    NEW RANGE

--- exp/webserver/run_experiment.sh
+++ exp/webserver/run_experiment.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-for (( i = 600; i <= 800; i += 25 )); do
+for (( i = 800; i <= 1000; i += 25 )); do
     echo "${i}/s"
     echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/1vegeta${i}.log"
 done
 
-for (( i = 775; i >= 600; i -= 25 )); do
+for (( i = 975; i >= 800; i -= 25 )); do
     echo "${i}/s"
     echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/2vegeta${i}.log"
 done
