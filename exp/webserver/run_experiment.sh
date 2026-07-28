a56622310d402fed1599d9002e739e6b0cb45193
m-sef <seth.l.moore0506@gmail.com>
2026-07-28 15:47:33 -0400

    Increased duration

--- exp/webserver/run_experiment.sh
+++ exp/webserver/run_experiment.sh
@@ -2,12 +2,12 @@
 
 for (( i = 800; i <= 1000; i += 25 )); do
     echo "${i}/s"
-    echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/1vegeta${i}.log"
+    echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="30s" -keepalive=false | vegeta encode -to=csv -output="/tmp/1vegeta${i}.log"
 done
 
 for (( i = 975; i >= 800; i -= 25 )); do
     echo "${i}/s"
-    echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/2vegeta${i}.log"
+    echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="30s" -keepalive=false | vegeta encode -to=csv -output="/tmp/2vegeta${i}.log"
 done
 
 exit 0
