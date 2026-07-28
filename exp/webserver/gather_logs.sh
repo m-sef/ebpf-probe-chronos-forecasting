c689815f89162d6d38754d7ca56d620c910c9595
m-sef <seth.l.moore0506@gmail.com>
2026-07-28 14:21:51 -0400

    Next exp

--- exp/webserver/gather_logs.sh
+++ exp/webserver/gather_logs.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VEGETA=slmoore@c220g2-011110.wisc.cloudlab.us
 WEBSERVER=slmoore@c220g2-011105.wisc.cloudlab.us
 
-scp $VEGETA:/tmp/vegeta*.log $SCRIPT_DIR/results/
+scp $VEGETA:/tmp/*vegeta*.log $SCRIPT_DIR/results/
 scp $WEBSERVER:/tmp/summary.log $SCRIPT_DIR/results/
 
 exit 0
--- exp/webserver/run_experiment.sh
+++ exp/webserver/run_experiment.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 for (( i = 10; i <= 100; i += 10 )); do
-    echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="$i/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/1vegeta${i}.log"
+    echo "${i}/s"
+    echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/1vegeta${i}.log"
 done
 
 for (( i = 90; i > 0; i -= 10 )); do
-    echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="$i/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/2vegeta${i}.log"
+    echo "${i}/s"
+    echo "GET http://10.10.1.1:8080/burn?burn=20" | vegeta attack -rate="${i}/s" -duration="10s" -keepalive=false | vegeta encode -to=csv -output="/tmp/2vegeta${i}.log"
 done
 
 exit 0
