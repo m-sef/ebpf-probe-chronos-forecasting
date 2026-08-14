cc03e3fc8ec0ebe97cb28d65f0d2b927e6e54116
m-sef <seth.l.moore0506@gmail.com>
2026-08-14 15:05:43 -0400

    pushing updated graph, added .ipynb to ignore languages

--- exp/increasing_rps/gather_logs.sh
+++ exp/increasing_rps/gather_logs.sh
@@ -2,9 +2,11 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-MASTER=slmoore@c220g2-011116.wisc.cloudlab.us
+MASTER=slmoore@c220g2-011125.wisc.cloudlab.us
 
-scp $MASTER:/local/ebpf-probe-chronos-forecasting/context.log $SCRIPT_DIR/results/
-scp $MASTER:/local/ebpf-probe-chronos-forecasting/prediction.log $SCRIPT_DIR/results/
+scp $MASTER:/tmp/context.log $SCRIPT_DIR/results/
+scp $MASTER:/tmp/prediction.log $SCRIPT_DIR/results/
+scp $MASTER:/tmp/rps_changes.log $SCRIPT_DIR/results/
+scp $MASTER:/tmp/vegeta.log $SCRIPT_DIR/results
 
 exit 0
